78f6dee3b4523b0cf8a1802deaa16bc19ca89668
AB Sharif <absharif.cs@gmail.com>
2022-06-09 19:18:19 +0600

    Deploying

--- clinic/urls.py
+++ clinic/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('website.urls')),
+    path('website/', include('website.urls')),
     path('clinic/', include('chamber.urls')),
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 ]
