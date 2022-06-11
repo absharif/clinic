caf6717bc9036cf4709cfc1d8758731243cfd46d
AB Sharif <absharif.cs@gmail.com>
2022-06-11 14:14:32 +0600

    update

--- clinic/urls.py
+++ clinic/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('website.urls')),  # This will be blank url
     path('clinic/', include('chamber.urls')),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
 ]
