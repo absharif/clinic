3a73a9ee78e69dd180bf36f0484cd006c4081de0
AB Sharif <absharif.cs@gmail.com>
2022-06-11 15:53:37 +0600

    update

--- clinic/urls.py
+++ clinic/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('website.urls')),  # This will be blank url
     path('clinic/', include('chamber.urls')),
     path('admin/', admin.site.urls, name='admin_url'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
