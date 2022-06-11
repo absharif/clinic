8fea773b5737926d2287315f689f78f3a744f6c7
AB Sharif <absharif.cs@gmail.com>
2022-06-11 14:09:53 +0600

    update

--- chamber/models.py
+++ chamber/models.py
@@ -1,6 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+class District(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+
+class BookNo(models.Model):
+    STATUS = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive')
+    )
+    name = models.CharField(max_length=120)
+    status = models.CharField(max_length=10, choices=STATUS)
+
+    def __str__(self):
+        return self.name
+
+
 class Profile(models.Model):
     BLOOD_GROUP = (
         ('B+', 'B+'),
@@ -20,16 +40,17 @@ class Profile(models.Model):
         ('Unmarried', 'Unmarried'),
     )
 
-    book_no = models.CharField(max_length=10)
+    book_no = models.CharField(max_length=10)  # BookNo, on_delete=models.CASCADE
     hive_id = models.CharField(max_length=10, null=True, blank=True)
     name = models.CharField(max_length=254)
     address = models.CharField(max_length=254, null=True, blank=True)
+    district = models.ForeignKey(District, on_delete=models.DO_NOTHING, null=True, blank=True)
     father_name = models.CharField(max_length=254, null=True, blank=True)
     mother_name = models.CharField(max_length=254, null=True, blank=True)
     marital_status = models.CharField(max_length=10, choices=MARITAL_STATUS)
     spouse_name = models.CharField(max_length=254, null=True, blank=True)
     age = models.DateField()
-    blood_group = models.CharField(max_length=10, choices=BLOOD_GROUP)
+    blood_group = models.CharField(max_length=10, choices=BLOOD_GROUP, blank=True, null=True)
     occupation = models.CharField(max_length=254, null=True, blank=True)
     contact_person = models.CharField(max_length=254, null=True, blank=True)
     phone = models.CharField(max_length=11, null=True, blank=True)
